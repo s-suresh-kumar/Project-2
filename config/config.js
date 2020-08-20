2b52103deb3915a56436e86cbb8ffb986082cc39
Suresh SK <suresh.uncbc.coding@gmail.com>
2020-08-20 19:33:25 -0400

    address Travis complaints

--- config/config.js
+++ config/config.js
@@ -5,17 +5,17 @@ module.exports = {
     password: process.env.DB_PASS,
     database: "passport_demo",
     host: process.env.DB_HOST,
-    dialect: "mysql",
+    dialect: "mysql"
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: "database_test",
     host: process.env.DB_HOST,
-    dialect: "mysql",
+    dialect: "mysql"
   },
   production: {
-    use_env_variable: "JAWSDB_URL",
-    dialect: "mysql",
-  },
+    useEnvVariable: "JAWSDB_URL",
+    dialect: "mysql"
+  }
 };
