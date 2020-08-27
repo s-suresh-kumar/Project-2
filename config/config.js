587b90e2a89b6a32d6a654eef498e0e49ad4e0e5
Kira Henkel <kira0226@gmail.com>
2020-08-26 22:36:34 -0400

    Updated username/password/version/routes

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: "passport_demo",
+    database: "tips_db",
     host: process.env.DB_HOST,
     dialect: "mysql"
   },
