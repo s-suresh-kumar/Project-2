3fed75600637a1d845182e85817ea9ed50542e7a
Suresh SK <suresh.uncbc.coding@gmail.com>
2020-09-01 12:06:09 -0400

    take care of some TravisCI issues, add thumbnail to README.md

--- models/restuarant.js
+++ models/restuarant.js
@@ -1,11 +1,11 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = (sequelize, DataTypes) => {
   // Creates a "Book" model that matches up with DB
   const Restuarant = sequelize.define("Restuarant", {
     name: DataTypes.STRING,
     location: DataTypes.STRING
   });
 
-  Restuarant.associate = function (models) {
+  Restuarant.associate = models => {
     Restuarant.hasMany(models.Waiter, {
       onDelete: "cascade"
     });
